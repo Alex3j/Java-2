26dbec12914e5ed0de1f8e32b8a28d81201ce1b1
Alexey Kudryavtsev <114296274+Alex3j@users.noreply.github.com>
2022-12-09 21:16:52 +0300

    Update lab2_2.java

--- lab2_2.java
+++ lab2_2.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 public class lab2_2 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        System.out.print("Введите пароль: ");
         String password = input.nextLine();
         boolean strength = password.matches("^(?=.*[0-9])(?=.*[A-Z])(?=.*[a-z])[0-9a-zA-Z_]{8,}$");
         if (strength==false) {
