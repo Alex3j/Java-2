ea76525d9e745d72f92bd301bce1a33fa0fb40a9
Alexey Kudryavtsev <114296274+Alex3j@users.noreply.github.com>
2022-12-09 21:18:06 +0300

    Update lab2_1.java

--- lab2_1.java
+++ lab2_1.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 public class lab2_1 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        System.out.print("Введите дату: ");
         String date = input.nextLine();
         boolean result = date.matches("^(0?[1-9]|[12][0-9]|3[0-1])/(0?[13578]|1[02])/(19[0-9][0-9]|[2-9][0-9][0-9][0-9])$|^(0?[1-9]|[12][0-9]|30)/(0?[469]|11)/(19[0-9][0-9]|[2-9][0-9][0-9][0-9])$|^(0?[1-9]|[12][0-8])/(0?2)/(19[0-9][0-9]|[2-9][0-9][0-9][0-9])$");
         boolean result2 = date.matches("^(0?[1-9]|[12][0-9]|3[0-1])\\.(0?[1-9]|1[012])\\.(19[0-9][0-9]|[2-9][0-9][0-9][0-9])$");
